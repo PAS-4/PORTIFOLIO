6cbd1b58c660500a766cc328f051f8320dfd966f
Patrese Ávila <64747567+PAS-4@users.noreply.github.com>
2021-02-02 21:35:26 -0300

    Initial commit

---
+++ _js/functions.js
@@ -0,0 +1,48 @@
+let _dicasDev = document.querySelector('#dicasDev')
+let _curriculum = document.querySelector('#curriculum')
+let _trabalhos = document.querySelector('#trabalhos')
+let _sobre = document.querySelector('#sobre')
+
+//DICASDEV
+function dicasDev(){
+    _dicasDev.style.display = 'block'
+    _curriculum.style.display = 'none'
+    _trabalhos.style.display = 'none'
+    _sobre.style.display = 'none'
+}
+
+//CURRÍCULUM
+function curriculum(){
+    _dicasDev.style.display = 'none'
+    _curriculum.style.display = 'block'
+    _trabalhos.style.display = 'none'
+    _sobre.style.display = 'none'
+}
+
+//TRABALHOS
+function trabalhos(){
+    _dicasDev.style.display = 'none'
+    _curriculum.style.display = 'none'
+    _trabalhos.style.display = 'block'
+    _sobre.style.display = 'none'
+}
+
+//SOBRE
+function sobre(){
+    _dicasDev.style.display = 'none'
+    _curriculum.style.display = 'none'
+    _trabalhos.style.display = 'none'
+    _sobre.style.display = 'block'
+}
+
+//qrCode
+function qrCode(foto, borderRadius){
+    const minhaFoto = document.querySelector('#foto')
+    minhaFoto.src = foto
+    minhaFoto.style.transition = '.5s'
+    if(borderRadius){
+        minhaFoto.style.borderRadius = '50%'
+    }else{
+        minhaFoto.style.borderRadius = '10%'
+    }
+}
